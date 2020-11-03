63a33f3f096b5c9907379846fea67f25c0698699
Maximilian Federle <max.federle@googlemail.com>
2020-11-03 21:04:14 +0100

    disable in-app checking for upgrades

--- scripts/launch-syncthing.sh
+++ scripts/launch-syncthing.sh
@@ -3,5 +3,6 @@
 realhome=$(getent passwd $UID | cut -d ':' -f 6)
 export HOME="$realhome"
 export XDG_CONFIG_HOME="$SNAP_USER_COMMON"
+export STNOUPGRADE="1"
 
 $SNAP/bin/syncthing "$@"
--- scripts/syncthing-daemon.sh
+++ scripts/syncthing-daemon.sh
@@ -2,5 +2,6 @@
 
 export HOME="$SNAP_COMMON"
 export XDG_CONFIG_HOME="$SNAP_COMMON"
+export STNOUPGRADE="1"
 
 $SNAP/bin/syncthing -no-browser
