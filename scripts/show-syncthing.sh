ba3d120dad8ea194db8fa31777627e5c96eaee70
Maximilian Federle <max.federle@googlemail.com>
2020-11-05 17:05:35 +0100

    fix race condition in show-ui & don't open two browser instances

--- scripts/show-syncthing.sh
+++ scripts/show-syncthing.sh
@@ -1,27 +1,38 @@
 #!/bin/bash -e
 
+first_run_marker="$SNAP_USER_COMMON/is-not-first-run"
+
 is_syncthing_running() {
-  pgrep -x syncthing -u $UID > /dev/null && return 0 || return 1
+  pgrep -x syncthing -u $UID > /dev/null
+}
+
+is_first_run() {
+  test ! -f "$first_run_marker"
 }
 
 setup_autostart() {
-  if [ ! -f "$SNAP_USER_COMMON/is-not-first-run" ]; then
-    is_first_run="yes"
+  user_wants_autostart=$($SNAP/bin/http-prompt $SNAP/prompt-for-autostart.html)
+  touch "$first_run_marker"
+  if [ "$user_wants_autostart" = "yes" ]; then
+    $SNAP/bin/enable-autostart.sh
   fi
-
-  if [ "$is_first_run" = "yes" ]; then
-    user_wants_autostart=$($SNAP/bin/http-prompt $SNAP/prompt-for-autostart.html)
-    touch "$SNAP_USER_COMMON/is-not-first-run"
-    if [ "$user_wants_autostart" = "yes" ]; then
-      $SNAP/bin/enable-autostart.sh
-    fi  
-  fi  
 }
 
-setup_autostart
+launch_syncthing() {
+  if is_syncthing_running; then 
+    $SNAP/bin/launch-syncthing.sh -browser-only
+  else
+    $SNAP/bin/launch-syncthing.sh \
+        -logfile="$SNAP_USER_COMMON/log.txt" \
+        "$@" \
+        &> /dev/null &
+  fi
+}
 
-if ! is_syncthing_running; then
-  $SNAP/bin/launch-syncthing.sh -logfile="$SNAP_USER_COMMON/log.txt" &> /dev/null &
+if is_first_run; then
+  # the autostart prompt will forward the user to syncthing
+  launch_syncthing -no-browser
+  setup_autostart
 else
-  $SNAP/bin/launch-syncthing.sh -browser-only
+  launch_syncthing
 fi
